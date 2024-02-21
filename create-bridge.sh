6c6a528e5a50ef4c6771152388650c52af1cd132
Hans Hübner <hans.huebner@gmail.com>
2024-02-21 06:02:51 +0000

    bridge-filtering beim starten der bridge einrichten

--- create-bridge.sh
+++ create-bridge.sh
@@ -5,3 +5,10 @@ set -e
 ip link add name br0 type bridge
 ip link set dev br0 up
 
+ebtables --flush
+ebtables -A FORWARD --protocol arp -j DROP
+ebtables -A FORWARD --protocol ipv4 -j DROP
+ebtables -A FORWARD --protocol ipv6 -j DROP
+ebtables -A INPUT --protocol arp -j DROP
+ebtables -A INPUT --protocol ipv4 -j DROP
+ebtables -A INPUT --protocol ipv6 -j DROP
