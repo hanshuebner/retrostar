b1500fac47ac183f89317f006075518fd3e127c9
Hans Hübner <hans.huebner@gmail.com>
2024-03-24 05:39:16 +0000

    fix multiple configurations per user in ovpn start script

--- start-openvpn-servers.sh
+++ start-openvpn-servers.sh
@@ -5,15 +5,15 @@ set -e
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR"
 
-psql -U retrostar -d retrostar -tAF' ' -c 'select u.name, oc.port_number from openvpn_configuration oc join "user" u on u.id = oc.user_id' | while read -r username port
+psql -U retrostar -d retrostar -tAF' ' -c 'select coalesce(oc.config_name, u.name), oc.port_number from openvpn_configuration oc join "user" u on u.id = oc.user_id' | while read -r config_name port
 do
-    pid_file="/var/run/openvpn/$username.pid"
+    pid_file="/var/run/openvpn/$config_name.pid"
     if [ -f "$pid_file" ] && [ -e "/proc/$(cat $pid_file)" ]
     then
         continue
     fi
-    echo "starting server for $username"
-    log_file="/var/log/openvpn/$username.log"
+    echo "starting server for $config_name"
+    log_file="/var/log/openvpn/$config_name.log"
     sudo openvpn --cd "ca" \
             --daemon \
             --verb 3 \
